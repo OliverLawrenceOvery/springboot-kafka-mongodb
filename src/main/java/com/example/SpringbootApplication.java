2a320fcf4b0f4a23ada22c31f7e79078ab6916b4
Oliver <olawrenceovery@deloitte.co.uk>
2021-12-16 12:25:26 +0000

    Added mongodb kubernetes deployment files and tracing capability

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import io.jaegertracing.Configuration;
+import io.opentracing.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringbootApplication {
@@ -9,4 +14,21 @@ public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
+
+//    @Bean
+//    public static Tracer getTracer() {
+//        Configuration.SamplerConfiguration samplerConfiguration = Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
+//        Configuration.ReporterConfiguration reporterConfiguration = Configuration.ReporterConfiguration.fromEnv().withLogSpans(true);
+//        Configuration config = new Configuration("jaeger tutorial").withSampler(samplerConfiguration).withReporter(reporterConfiguration);
+//        return config.getTracer();
+//    }
+//
+//    @PostConstruct
+//    public void setProperty() {
+//        System.setProperty("JAEGER_REPORTER_LOG_SPANS", "true");
+//    }
 }
+
+
+
+
--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -4,16 +4,29 @@ import com.example.domain.Product;
 import com.example.repository.ProductRepository;
 import com.example.service.ProductService;
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import io.jaegertracing.Tracer;
+import io.jaegertracing.reporters.RemoteReporter;
+import io.jaegertracing.samplers.ConstSampler;
+import io.jaegertracing.senders.UdpSender;
+import io.opentracing.Span;
+import io.opentracing.SpanContext;
+import io.opentracing.propagation.TextMapInjectAdapter;
 import org.bson.Document;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.opentracing.util.AutoFinishScopeManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
+import javax.annotation.PostConstruct;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.opentracing.propagation.Format.Builtin.HTTP_HEADERS;
 
 @RestController
 @Validated
@@ -24,12 +37,33 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    private Tracer tracer;
+
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
+    @PostConstruct
+    public void init() {
+        tracer = new Tracer.Builder("spring-product-service")
+                .withReporter(new RemoteReporter.Builder()
+                        .withSender(new UdpSender("127.0.0.1", 6831, 0))
+                        .build())
+                .withSampler(new ConstSampler(true))
+                .withScopeManager(new AutoFinishScopeManager())
+                .build();
+    }
+
     @PostMapping
     public Mono<Boolean> saveProduct(@RequestBody Product product) {
+        tracer.buildSpan("save-product").startActive(true);
+        SpanContext context = tracer.activeSpan().context();
+        tracer.inject(context, HTTP_HEADERS, new TextMapInjectAdapter(new HashMap<>()));
+
+        log.info("Saving product");
+        Mono<Boolean> isProductSaved = productService.saveProduct(product);
+
+        tracer.scopeManager().active().close();
         return productService.saveProduct(product);
     }
 
@@ -40,6 +74,14 @@ public class ProductController {
 
     @GetMapping("/all")
     public Publisher<Document> getAllProducts() {
-        return productService.getAll();
+        HashMap<String, String> map = new HashMap<>();
+
+        tracer.buildSpan("get-all-products").startActive(true);
+        SpanContext context = tracer.activeSpan().context();
+        tracer.inject(context, HTTP_HEADERS, new TextMapInjectAdapter(new HashMap<>()));
+
+        Publisher<Document> products = productService.getAll();
+        tracer.scopeManager().active().close();
+        return products;
     }
 }
